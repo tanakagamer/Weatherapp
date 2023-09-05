629b3ab2f9b0ce72db6472fbeab3eee8a4371966
Vandré Tanaka <59923817+tanakagamer@users.noreply.github.com>
2023-09-05 14:31:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "4edd80aece6dec868935f40ea8041f32"; // API key for OpenWeatherMap API
+const API_KEY = "YOUR_API_KEY"; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
